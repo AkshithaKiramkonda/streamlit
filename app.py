5510317201486fc3ac7e832fce77bf23e9611552
AkshithaKiramkonda <167959366+AkshithaKiramkonda@users.noreply.github.com>
2024-07-21 18:26:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title('hello akshitha how are u!!')
+st.header('header')
+st.subheader('subheader')
+st.text('Text')
+# this is how normal text looks like
+
+st.markdown('just a simple text')
+st.markdown('# just a simple text') # by using '#' it gives the title size"H1"
+st.markdown('## just a simple text')
+st.markdown('G**F**G') # makes f bold
+st.markdown('G*F*G') # f is itallic
+st.markdown('G***F***G') # bold+itallic
+st.markdown('- First point') # bullet point
+st.markdown('- second point')
+st.write('text') # same like text or paragrap h tag
+st.write('range(1,10)') # same text
+st.write(range(1,10)) # displays in color
